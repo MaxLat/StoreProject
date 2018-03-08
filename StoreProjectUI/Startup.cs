3ade1e398476ee8a3add666f6cf39b519a1a90fc
unknown <dahaka18@gmail.com>
2018-03-08 18:40:17 +0100

    start store project

---
+++ StoreProjectUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StoreProjectUI.Startup))]
+namespace StoreProjectUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
